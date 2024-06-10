e572834388174aaf1c64c13721145563c7f56848
amith <amith@saastaq.io>
2024-06-11 01:29:03 +0530

    added env file

--- Client/dinujaya_client/src/App.js
+++ Client/dinujaya_client/src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Login from './pages/Login'
-import Register from './pages/Register.js'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register.js';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
-       <Routes>
-        <Route path='/' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-       </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <ToastContainer />
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
